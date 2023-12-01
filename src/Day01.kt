c61457ebcc9ba6f5798377f186bf3c62e9f40dfc
Zinary <ram7953@gmail.com>
2023-12-01 15:45:46 +0530

    Solution for Day 1 - Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun getCalibrationDigitFromString(calibrationText: String): Int {
+        var firstDigit = 0
+        var lastDigit = 0
+
+        for (it in calibrationText) {
+            if (it.isDigit()) {
+                val digit = it.digitToInt()
+                if (firstDigit == 0) {
+                    firstDigit = digit
+                } else {
+                    lastDigit = digit
+                }
+
+                if (lastDigit == 0) {
+                    lastDigit = firstDigit
+                }
+            }
+        }
+
+        return "$firstDigit$lastDigit".toInt()
     }
+
+
+    fun part1(input: List<String>): Int {
+        return input.sumOf(::getCalibrationDigitFromString)
+    }
+
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    part1(testInput).println()
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
